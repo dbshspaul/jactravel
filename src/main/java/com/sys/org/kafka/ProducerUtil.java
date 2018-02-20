f65c75eb3e90edcc6a97aa915c97ef7c2fa5d10c
dbshspaul <dbshspaul@gmail.com>
2018-02-20 07:44:16 +0530

    implementation kafka

--- src/main/java/com/sys/org/kafka/ProducerUtil.java
+++ src/main/java/com/sys/org/kafka/ProducerUtil.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
@@ -11,21 +12,25 @@ public class ProducerUtil {
     private ProducerUtil() {
     }
 
-    private static ProducerUtil getInstance() {
-        return new ProducerUtil();
+    private static class SingletonHelper{
+        private static final ProducerUtil INSTANCE = new ProducerUtil();
     }
 
-    private Producer getProducer() {
+    public static ProducerUtil getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+
+    private Producer<String, String> getProducer() {
         Properties configProperties = new Properties();
         configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
-        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-        return new KafkaProducer(configProperties);
+        return new KafkaProducer<String, String>(configProperties);
     }
 
-    public static void sendMessage(String topicName, String message) {
-        Producer producer = getInstance().getProducer();
+    public void sendMessage(String topicName, String message) {
+        Producer<String, String> producer = getInstance().getProducer();
         try {
             ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName, message);
             producer.send(rec);
---
+++ src/main/java/com/sys/org/spring/controller/KafkaController.java
@@ -0,0 +1,66 @@
+package com.sys.org.spring.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sys.org.spring.idclass.ContractPK;
+import com.sys.org.spring.idclass.PropertyPK;
+import com.sys.org.spring.idclass.RatePK;
+import com.sys.org.spring.idclass.RoomPK;
+import com.sys.org.spring.model.Contract;
+import com.sys.org.spring.model.Property;
+import com.sys.org.spring.model.Rate;
+import com.sys.org.spring.model.Room;
+import org.apache.ignite.IgniteCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class KafkaController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaController.class);
+    @Autowired
+    IgniteCache<ContractPK, Contract> contractCache;
+
+    @Autowired
+    IgniteCache<PropertyPK, Property> propertyCache;
+
+    @Autowired
+    IgniteCache<RatePK, Rate> rateCache;
+
+    @Autowired
+    IgniteCache<RoomPK, Room> roomCache;
+
+
+    @KafkaListener(topics = "${kafka.topic.name}")
+    public void receive(String payload) {
+        LOGGER.info("New payload received from kafka");
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Contract contract = mapper.readValue(payload, Contract.class);
+            contractCache.put(contract.getContractPK(), contract);
+            LOGGER.info("Contract save/updated successfully.");
+        } catch (IOException e) {
+        }
+        try {
+            Room room = mapper.readValue(payload, Room.class);
+            roomCache.put(room.getRoomPK(), room);
+            LOGGER.info("Room save/updated successfully.");
+        } catch (IOException e) {
+        }
+        try {
+            Rate rate = mapper.readValue(payload, Rate.class);
+            rateCache.put(rate.getRatePK(), rate);
+            LOGGER.info("Rate save/updated successfully.");
+        } catch (IOException e) {
+        }
+        try {
+            Property property = mapper.readValue(payload, Property.class);
+            propertyCache.put(property.getPropertyPK(), property);
+            LOGGER.info("Property save/updated successfully.");
+        } catch (IOException e) {
+        }
+    }
+}
